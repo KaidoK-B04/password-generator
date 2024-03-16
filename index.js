15c33e577b4aff376603f62aa11f26494d2ec5ce
KaidoK-B04 <131766275+KaidoK-B04@users.noreply.github.com>
2024-03-16 05:19:43 +0100

    Version 0.1

---
+++ index.js
@@ -0,0 +1,138 @@
+const characters1 = [
+  "🐉",
+  "🐥",
+  "🐊",
+  "💩",
+  "🦍",
+  "🐢",
+  "🐩",
+  "🦭",
+  "🦀",
+  "🐝",
+  "🤖",
+  "🐘",
+  "🐸",
+  "🕷",
+  "🐆",
+  "🦕",
+  "🦁",
+];
+const characters2 = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passwordOne = document.getElementById("password-one");
+let passwordTwo = document.getElementById("password-two");
+
+function generatePass() {
+  let password = "";
+  for (let i = 0; i < 15; i++) {
+    const randomIndex = Math.floor(Math.random() * characters1.length);
+    password += characters1[randomIndex];
+  }
+  passwordOne.textContent = password;
+  password = "";
+  for (let i = 0; i < 15; i++) {
+    const randomIndex = Math.floor(Math.random() * characters2.length);
+    password += characters2[randomIndex];
+  }
+  passwordTwo.textContent = password;
+}
+
+//COPY TO CLIPBOARD
+function copyToClipboard(element) {
+  const text = element.textContent;
+  navigator.clipboard.writeText(text);
+
+  alert("Password copied to clipboard: " + text);
+}
